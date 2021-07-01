169c0ca64e8d970f2a1975f37d3d641abba7944a
Amitdahari <50590138+Amitdahari@users.noreply.github.com>
2021-07-01 16:01:16 +0300

    fixed table

--- src/app/client-list/client-list.component.ts
+++ src/app/client-list/client-list.component.ts
@@ -7,23 +7,17 @@ interface Userchoice {
 }
 
 export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
+  id: string;
+  phone: string;
+  firstName: string;
+  lastName: string;
+  dateOfbirth: Date;
 }
 
 const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+  {id: '111',phone:'123',firstName:'Amit',lastName:'Dahari',dateOfbirth:new Date("01-07-2021") },
+  {id: '222',phone:'0524336841',firstName:'Ben',lastName:'Cohen',dateOfbirth:new Date("01-07-1996") },
+  {id: '333',phone:'0524113547',firstName:'Yaniv',lastName:'Tsioni',dateOfbirth:new Date("01-07-1997") },
 ];
 
 @Component({
@@ -39,12 +33,12 @@ export class ClientListComponent implements OnInit {
     
   ];
 
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
-  dataSource = ELEMENT_DATA;
+  displayedColumns: string[] = ['Id', 'Phone', 'First Name', 'Last Name', 'Date of Birth'];
+  clientDataSource = ELEMENT_DATA;
   clickedRows = new Set<PeriodicElement>();
 
   constructor(private fb: FormBuilder) { }
-clientForm:FormGroup;
+  clientForm: FormGroup = new FormGroup({});
   ngOnInit(): void {
     this.clientForm = this.fb.group({
       searchTerm:['',[Validators.required]],
